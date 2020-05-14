b4735e50de523ab3dcb414b39fc4eae3f0d5c029
Huseyn Mikayil <huseyn-mikayil@mail.ru>
2020-05-14 21:27:08 +0400

    Bootcamp model was modified

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -43,11 +43,13 @@ const BootcampSchema = new mongoose.Schema(
             // GeoJSON Point
             type: {
                 type: String,
-                enum: ['Point']
+                enum: ['Point'],
+                required: true
             },
             coordinates: {
                 type: [Number],
-                index: '2dsphere'
+                index: '2dsphere',
+                required: true
             },
             formattedAddress: String,
             street: String,
@@ -98,57 +100,8 @@ const BootcampSchema = new mongoose.Schema(
         createdAt: {
             type: Date,
             default: Date.now
-        },
-        user: {
-            type: mongoose.Schema.ObjectId,
-            ref: 'User',
-            required: true
         }
-    },
-    {
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true }
     }
 );
-
-// Create bootcamp slug from the name
-BootcampSchema.pre('save', function (next) {
-    this.slug = slugify(this.name, { lower: true });
-    next();
-});
-
-// Geocode & create location field
-BootcampSchema.pre('save', async function (next) {
-    const loc = await geocoder.geocode(this.address);
-    this.location = {
-        type: 'Point',
-        coordinates: [loc[0].longitude, loc[0].latitude],
-        formattedAddress: loc[0].formattedAddress,
-        street: loc[0].streetName,
-        city: loc[0].city,
-        state: loc[0].stateCode,
-        zipcode: loc[0].zipcode,
-        country: loc[0].countryCode
-    };
-
-    // Do not save address in DB
-    this.address = undefined;
-    next();
-});
-
-// Cascade delete courses when a bootcamp is deleted
-BootcampSchema.pre('remove', async function (next) {
-    console.log(`Courses being removed from bootcamp ${this._id}`);
-    await this.model('Course').deleteMany({ bootcamp: this._id });
-    next();
-});
-
-// Reverse populate with virtuals
-BootcampSchema.virtual('courses', {
-    ref: 'Course',
-    localField: '_id',
-    foreignField: 'bootcamp',
-    justOne: false
-});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
