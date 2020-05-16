d3a2adad55d1a1f0d1f93c99a2b7d50228d73a35
Huseyn Mikayil <huseyn-mikayil@mail.ru>
2020-05-16 16:55:54 +0400

    basic bootcamp crud was created

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const slugify = require('slugify');
-const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema(
     {
@@ -43,13 +41,11 @@ const BootcampSchema = new mongoose.Schema(
             // GeoJSON Point
             type: {
                 type: String,
-                enum: ['Point'],
-                required: true
+                enum: ['Point']
             },
             coordinates: {
                 type: [Number],
-                index: '2dsphere',
-                required: true
+                index: '2dsphere'
             },
             formattedAddress: String,
             street: String,
